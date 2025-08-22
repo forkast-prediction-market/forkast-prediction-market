f6651261c215b7f2177db0be5b670642f90b9764
Bruno Tomé <ibrunotome@gmail.com>
2025-08-22 20:40:58 -0300

    refactor: move layout to layout.tsx and skeleton to loading.tsx

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next'
 import { redirect } from 'next/navigation'
+import React from 'react'
+import SettingsTabsClientComponent from '@/app/settings/_components/SettingsTabsClientComponent'
 import { getCurrentUser } from '@/lib/db/users'
-import SettingsContent from './_components/SettingsContent'
 
 export const metadata: Metadata = {
   title: 'Settings',
@@ -14,13 +15,5 @@ export default async function SettingsPage() {
     redirect('/')
   }
 
-  return (
-    <main className="container py-8">
-      <div className="mx-auto max-w-6xl">
-        <div className="grid gap-8 lg:grid-cols-[240px_1fr] lg:gap-16">
-          <SettingsContent user={user} />
-        </div>
-      </div>
-    </main>
-  )
+  return <SettingsTabsClientComponent user={user} />
 }
