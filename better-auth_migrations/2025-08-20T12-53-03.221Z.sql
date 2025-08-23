ef436837289163a64c3d8c1a3336792ac8e713bd
Bruno Tomé <ibrunotome@gmail.com>
2025-08-23 13:43:50 -0300

    refactor: use plural and snake_case convention for auth schema

--- better-auth_migrations/2025-08-20T12-53-03.221Z.sql
+++ better-auth_migrations/2025-08-20T12-53-03.221Z.sql
@@ -1,61 +1,63 @@
-create table "user"
+create table "users"
 (
-  "id"            text                                not null primary key,
-  "name"          text                                not null,
-  "email"         text                                not null unique,
-  "emailVerified" boolean                             not null,
-  "image"         text,
-  "createdAt"     timestamp default current_timestamp not null,
-  "updatedAt"     timestamp default current_timestamp not null
+  "id"             text                                not null primary key,
+  "address"        text unique,
+  "username"       text unique,
+  "email"          text                                not null unique,
+  "email_verified" boolean                             not null default false,
+  "bio"            text,
+  "image"          text,
+  "created_at"     timestamp default current_timestamp not null,
+  "updated_at"     timestamp default current_timestamp not null
 );
 
-create table "session"
+create table "sessions"
 (
-  "id"        text      not null primary key,
-  "expiresAt" timestamp not null,
-  "token"     text      not null unique,
-  "createdAt" timestamp not null,
-  "updatedAt" timestamp not null,
-  "ipAddress" text,
-  "userAgent" text,
-  "userId"    text      not null references "user" ("id") on delete cascade
+  "id"         text      not null primary key,
+  "expires_at" timestamp not null,
+  "token"      text      not null unique,
+  "ip_address" text,
+  "user_agent" text,
+  "user_id"    text      not null references "users" ("id") on delete cascade,
+  "created_at" timestamp not null,
+  "updated_at" timestamp not null
 );
 
-create table "account"
+create table "accounts"
 (
-  "id"                    text      not null primary key,
-  "accountId"             text      not null,
-  "providerId"            text      not null,
-  "userId"                text      not null references "user" ("id") on delete cascade,
-  "accessToken"           text,
-  "refreshToken"          text,
-  "idToken"               text,
-  "accessTokenExpiresAt"  timestamp,
-  "refreshTokenExpiresAt" timestamp,
-  "scope"                 text,
-  "password"              text,
-  "createdAt"             timestamp not null,
-  "updatedAt"             timestamp not null
+  "id"                       text      not null primary key,
+  "account_id"               text      not null,
+  "provider_id"              text      not null,
+  "user_id"                  text      not null references "users" ("id") on delete cascade,
+  "access_token"             text,
+  "refresh_token"            text,
+  "id_token"                 text,
+  "access_token_expires_at"  timestamp,
+  "refresh_token_expires_at" timestamp,
+  "scope"                    text,
+  "password"                 text,
+  "created_at"               timestamp not null,
+  "updated_at"               timestamp not null
 );
 
-create table "verification"
+create table "verifications"
 (
   "id"         text      not null primary key,
   "identifier" text      not null,
   "value"      text      not null,
-  "expiresAt"  timestamp not null,
-  "createdAt"  timestamp default current_timestamp,
-  "updatedAt"  timestamp default current_timestamp
+  "expires_at" timestamp not null,
+  "created_at" timestamp default current_timestamp,
+  "updated_at" timestamp default current_timestamp
 );
 
-create table "walletAddress"
+create table "wallet_address"
 (
-  "id"        text      not null primary key,
-  "userId"    text      not null references "user" ("id") on delete cascade,
-  "address"   text      not null,
-  "chainId"   integer   not null,
-  "isPrimary" boolean   not null,
-  "createdAt" timestamp not null
+  "id"         text      not null primary key,
+  "user_id"    text      not null references "users" ("id") on delete cascade,
+  "address"    text      not null,
+  "chain_id"   integer   not null,
+  "is_primary" boolean   not null,
+  "created_at" timestamp not null
 );
 
-grant all privileges on table "user" to service_role;
+grant all privileges on table "users" to service_role;
