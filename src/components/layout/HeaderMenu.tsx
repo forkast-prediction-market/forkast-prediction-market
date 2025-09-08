a9f9cfcdd6ff2c98104fc02b1324ac1c8bf5f84e
Bruno Tomé <ibrunotome@gmail.com>
2025-09-08 11:27:53 -0300

    fix: stuck on connecting

--- src/components/layout/HeaderMenu.tsx
+++ src/components/layout/HeaderMenu.tsx
@@ -1,18 +1,35 @@
 'use client'
 
 import { useAppKit, useAppKitAccount } from '@reown/appkit/react'
+import { useEffect } from 'react'
 import HeaderDropdownUserMenuAuth from '@/components/layout/HeaderDropdownUserMenuAuth'
 import HeaderDropdownUserMenuGuest from '@/components/layout/HeaderDropdownUserMenuGuest'
 import HeaderNotifications from '@/components/layout/HeaderNotifications'
 import HeaderPortfolio from '@/components/layout/HeaderPortfolio'
 import { Button } from '@/components/ui/button'
 import { Skeleton } from '@/components/ui/skeleton'
+
 import { useClientMounted } from '@/hooks/useClientMounted'
 
 export default function HeaderMenu() {
   const isMounted = useClientMounted()
   const { open } = useAppKit()
   const { isConnected, status } = useAppKitAccount()
+
+  useEffect(() => {
+    let timeout: number
+    if (status === 'connecting') {
+      timeout = window.setTimeout(() => {
+        Object.keys(localStorage).forEach((key) => {
+          if (key.startsWith('@appkit')) {
+            localStorage.removeItem(key)
+          }
+        })
+        location.reload()
+      }, 15000)
+    }
+    return () => clearTimeout(timeout)
+  }, [status])
 
   if (!isMounted || status === 'connecting') {
     return (
