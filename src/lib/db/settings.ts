3948f6abdfcfa8907679ddf0035c7af310e10424
Bruno Tomé <ibrunotome@gmail.com>
2025-10-20 21:07:28 -0300

    refactor: wip on migration of queries to drizzle orm (settings)

--- src/lib/db/settings.ts
+++ src/lib/db/settings.ts
@@ -1,44 +1,55 @@
+import { sql } from 'drizzle-orm'
 import { unstable_cacheTag as cacheTag, revalidateTag } from 'next/cache'
 import { cacheTags } from '@/lib/cache-tags'
-import { supabaseAdmin } from '@/lib/supabase'
+import { settings } from '@/lib/db/schema'
+import { db } from '@/lib/drizzle'
 
 export const SettingsRepository = {
   async getSettings() {
     'use cache'
     cacheTag(cacheTags.settings)
 
-    const { data, error } = await supabaseAdmin
-      .from('settings')
-      .select('group, key, value, updated_at')
+    try {
+      const data = await db.select({
+        group: settings.group,
+        key: settings.key,
+        value: settings.value,
+        updated_at: settings.updated_at,
+      }).from(settings)
 
-    if (error) {
-      return { data: null, error }
+      const settingsByGroup: Record<string, Record<string, { value: string, updated_at: string }>> = {}
+
+      for (const setting of data) {
+        settingsByGroup[setting.group] ??= {}
+        settingsByGroup[setting.group][setting.key] = {
+          value: setting.value,
+          updated_at: setting.updated_at.toISOString(),
+        }
+      }
+
+      return { data: settingsByGroup, error: null }
     }
-
-    const settingsByGroup: Record<string, Record<string, { value: string, updated_at: string }>> = {}
-
-    data?.forEach((setting) => {
-      if (!settingsByGroup[setting.group]) {
-        settingsByGroup[setting.group] = {}
-      }
-      settingsByGroup[setting.group][setting.key] = {
-        value: setting.value,
-        updated_at: setting.updated_at,
-      }
-    })
-
-    return { data: settingsByGroup, error: null }
+    catch (e: any) {
+      return { data: null, e }
+    }
   },
 
-  async updateSettings(settings: Array<{ group: string, key: string, value: string }>) {
-    const { data, error } = await supabaseAdmin
-      .from('settings')
-      .upsert(settings, { onConflict: 'group,key' })
-      .select('group, key, value, updated_at')
-
-    if (error) {
-      return { data: null, error }
-    }
+  async updateSettings(settingsArray: Array<{ group: string, key: string, value: string }>) {
+    const data = await db
+      .insert(settings)
+      .values(settingsArray)
+      .onConflictDoUpdate({
+        target: [settings.group, settings.key],
+        set: {
+          value: sql`EXCLUDED.value`,
+        },
+      })
+      .returning({
+        group: settings.group,
+        key: settings.key,
+        value: settings.value,
+        updated_at: settings.updated_at,
+      })
 
     revalidateTag(cacheTags.settings)
 
