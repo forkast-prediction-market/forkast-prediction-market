c0726464523854f06d3b150261beeb5c20baaefa
Bruno Tomé <ibrunotome@gmail.com>
2025-10-22 13:47:21 -0300

    fix: reuse drizzle client

--- src/lib/drizzle.ts
+++ src/lib/drizzle.ts
@@ -4,13 +4,10 @@ import * as schema from './db/schema'
 
 const globalForDb = globalThis as unknown as {
   client: postgres.Sql | undefined
-  db: ReturnType<typeof drizzle> | undefined
 }
 
 const client = globalForDb.client ?? postgres(process.env.POSTGRES_URL!)
-const db = globalForDb.db ?? drizzle(client, { schema })
 
 globalForDb.client = client
-globalForDb.db = db
 
-export { db }
+export const db = drizzle(client, { schema })
