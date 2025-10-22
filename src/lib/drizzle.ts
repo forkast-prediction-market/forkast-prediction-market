4de2d2207e4bf5d7082007f87ce0c8cf7143bdc5
Bruno Tomé <ibrunotome@gmail.com>
2025-10-22 13:35:51 -0300

    fix: prevent multiple drizzle or db instances

--- src/lib/drizzle.ts
+++ src/lib/drizzle.ts
@@ -2,5 +2,15 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from './db/schema'
 
-const client = postgres(process.env.POSTGRES_URL!)
-export const db = drizzle(client, { schema })
+const globalForDb = globalThis as unknown as {
+  client: postgres.Sql | undefined
+  db: ReturnType<typeof drizzle> | undefined
+}
+
+const client = globalForDb.client ?? postgres(process.env.POSTGRES_URL!)
+const db = globalForDb.db ?? drizzle(client, { schema })
+
+globalForDb.client = client
+globalForDb.db = db
+
+export { db }
