cba0e4892edd8b8b3217e09573fb95d86dcd3705
Bruno Tomé <ibrunotome@gmail.com>
2026-01-19 20:59:15 -0300

    feat: integrate intl provider

---
+++ src/components/IntlProvider.tsx
@@ -0,0 +1,21 @@
+import type { ReactNode } from 'react'
+import { NextIntlClientProvider } from 'next-intl'
+import { getLocale, getMessages, getTimeZone } from 'next-intl/server'
+
+interface IntlProviderProps {
+  children: ReactNode
+}
+
+export default async function IntlProvider({ children }: IntlProviderProps) {
+  const [locale, messages, timeZone] = await Promise.all([
+    getLocale(),
+    getMessages(),
+    getTimeZone(),
+  ])
+
+  return (
+    <NextIntlClientProvider locale={locale} messages={messages} timeZone={timeZone}>
+      {children}
+    </NextIntlClientProvider>
+  )
+}
