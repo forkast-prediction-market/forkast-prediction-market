cbca8a04e489a3732bab92af2cc61a1865bd2082
Bruno Tomé <ibrunotome@gmail.com>
2026-01-26 14:16:18 -0300

    fix: cactch all page

--- src/app/[locale]/[...rest]/page.tsx
+++ src/app/[locale]/[...rest]/page.tsx
@@ -1,19 +1,5 @@
-import { hasLocale } from 'next-intl'
 import { notFound } from 'next/navigation'
-import { loadEnabledLocales } from '@/i18n/locale-settings'
-import { routing } from '@/i18n/routing'
 
-export default async function LocaleCatchAll({ params }: PageProps<'/[locale]/[...rest]'>) {
-  const { locale } = await params
-
-  if (!hasLocale(routing.locales, locale)) {
-    notFound()
-  }
-
-  const enabledLocales = await loadEnabledLocales()
-  if (!enabledLocales.includes(locale)) {
-    notFound()
-  }
-
+export default function CatchAllPage() {
   notFound()
 }
